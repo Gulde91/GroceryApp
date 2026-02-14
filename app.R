6f39b24aebec155a08999a27b26d1d368e37e7e7
Gulde91 <alexguldbrand@gmail.com>
2026-02-14 10:12:18 +0100

    ..

--- app.R
+++ app.R
@@ -873,7 +873,7 @@ server <- function(input, output, session) {
     
     path <- paste0("./data/indkobssedler/indkobsseddel_", gsub("-", "", Sys.Date()), ".rda")
     save(df, file = path)
-    shinyjs::runjs("showCopyToast(\"Indkøbsseddel gemt ✔\", \"blue\")")
+    runjs("showCopyToast(\"Indkøbsseddel gemt ✔\", \"blue\")")
     
   })
   
